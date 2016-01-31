dfe1479f2c0fe1e7eee5d57b70814efa746741f9
abdelraouf <rofarewsh@gmail.com>
2016-01-31 13:51:34 +0200

    Handling Channel related messages in GO using goroutines and socket API in gorilla toolkit

--- src/github.com/Abdel-Raouf/rtsupport/junk.go
+++ src/github.com/Abdel-Raouf/rtsupport/junk.go
@@ -3,13 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/mitchellh/mapstructure"
 )
 
 type Message struct {
-	Name string 
-	Data interface{}
+	Name string 		`json:"name"` 
+	Data interface{}	`json:"data"`
 }
 
+type Channel struct {
+	Id string	`json:"id"`
+	Name string	`json:"name"`
+}
 
 func main() {
 	recRawMsg := []byte(`{"name":"channel add",` + `"data":{"name":"Hardware Support"}}`)
@@ -20,4 +25,37 @@ func main() {
 		return
 	}
 	fmt.Printf("%#v\n", recMessage)
+
+	if recMessage.Name == "channel add" {
+		channel, err := addChannel(recMessage.Data)
+		var sendMessage Message
+		sendMessage.Name = "channel add"
+		sendMessage.Data = channel
+		sendRawMsg, err := json.Marshal(sendMessage)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf(string(sendRawMsg))
+	}
 }
+
+func addChannel(data interface{}) (Channel, error) {
+	var channel Channel
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		return channel, err
+	}
+	channel.Id = "1"
+	fmt.Printf("%#v\n", channel)
+	return channel, nil
+}
+
+
+
+
+
+
+
+
+
